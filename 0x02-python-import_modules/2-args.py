6af659b0a41f071a1530c2018ac36f8d07796365
Salome-N <njengasalome87@gmail.com>
2023-11-02 11:33:52 -0700

    Print arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    c = len(sys.argv) - 1
+
+    if c == 0:
+        print("0 argument.")
+    elif c == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(c))
+    for a in range(c):
+        print("{}: {}".format(a + 1, sys.argv[a + 1]))
