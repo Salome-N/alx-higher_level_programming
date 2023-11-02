9a4e13e95bdcf09e438d4761da62c83cdb45928e
Salome-N <njengasalome87@gmail.com>
2023-11-02 11:36:47 -0700

    Print arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     c = len(sys.argv) - 1
 
     if c == 0:
-        print("0 argument.")
+        print("0 arguments.")
     elif c == 1:
         print("1 argument:")
     else:
