1c914c297c10bacf16d1a9174d513ac6a75c41c4
Salome-N <njengasalome87@gmail.com>
2023-11-20 08:39:13 -0800

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    count = 0
     try:
+        count = 0
         if (count < x):
             print("{}".format(my_list[count]), end="")
             count += 1
