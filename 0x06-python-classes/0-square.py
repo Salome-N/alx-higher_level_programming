5ab12ce72359a6af60119604caaa873a0a0328f7
Salome-N <njengasalome87@gmail.com>
2023-11-21 07:49:01 -0800

    empy sq class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 
-""" Define a square """
+"""Define a square
+
+    Create an empty class named square.
+
+"""
 
 class square:
-    """ empty class Square """
+    """Class square
+
+    An empty class.
+
+    Attributes:
+        None
+
+    """
     pass
