9b3513be1e78fa4bb4e3993be5a1a504e21450b4
Salome-N <njengasalome87@gmail.com>
2023-11-21 08:01:11 -0800

    Init sq class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Defines a square."""
+
+class Square:
+    """Class square."""
+    def __init__(self, size):
+        """Initializes a square
+
+            Args:
+                size (int): size of square.
+        """
+        self.__size = size
