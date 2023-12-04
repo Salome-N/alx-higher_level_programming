1ad0d80e81f5bea309dc606bf59f115e3743adfa
Salome-N <njengasalome87@gmail.com>
2023-12-04 10:27:39 -0800

    Fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,7 +3,7 @@
 """Define class Mylist with inherited list."""
 
 
-class Mylist(list):
+class MyList(list):
     """Custom list class."""
     def print_sorted(self):
         """Sorted printing in ascending order."""
