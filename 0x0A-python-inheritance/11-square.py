b23eadfb340c0141e20504240c3bd56f0bd61e6e
Salome-N <njengasalome87@gmail.com>
2023-12-04 12:45:17 -0800

    Square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a subclass square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size):
+        """Initialize square
+
+        Args:
+            size (int): size of the square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """calculates area of square"""
+        return (self.__size ** 2)
+
+    def __str__(self):
+        """string representationof square"""
+        return ("[Square] {0}/{0}".format(self.__size))
