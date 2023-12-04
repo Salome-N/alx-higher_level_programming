f393eca172fd919cb3d2f524ee01502359933574
Salome-N <njengasalome87@gmail.com>
2023-12-04 09:48:43 -0800

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Define the lookup function."""
+
+def lookup(obj):
+    """Returns list of available attributes and methods of an object.
+        
+    Args:
+        obj (object): an object
+
+    """
+    return dir(obj)
