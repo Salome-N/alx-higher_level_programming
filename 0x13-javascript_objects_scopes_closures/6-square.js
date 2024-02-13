e835f6db7e8f84c93977645d91727594c0170976
Salome-N <njengasalome87@gmail.com>
2024-02-13 15:15:20 -0800

    Square class js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+module.exports = class Square extends require('./5-square.js') {
+  charprint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let l = 0; l < this.height; l++) console.log(c.repeat(this.width));
+    }
+  }
+};
