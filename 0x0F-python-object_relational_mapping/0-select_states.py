d080a1a9bf643da7369a5c99ffd711370253894b
Salome-N <njengasalome87@gmail.com>
2024-02-23 15:15:29 -0800

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     port = 3306
     db = MySQLdb.connect(host, user, passwd, database, port)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states`")
+    cursor.execute("SELECT * FROM `states` ORDER BY states.id ASC")
     [print(state) for state in cursor.fetchall()]
