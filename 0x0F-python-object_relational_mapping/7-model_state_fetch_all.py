f54ac6815242237fee5df98994d005297004661a
Salome-N <njengasalome87@gmail.com>
2024-02-23 16:40:54 -0800

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,9 +7,8 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    uri_db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-    )
+    uri_db = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+             sys.argv[1], sys.argv[2], sys.argv[3])
     engine = create_engine(uri_db, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
