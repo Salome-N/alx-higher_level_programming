f60d7d1326c5aff5c5587c017ebfd76dc75acea1
Salome-N <njengasalome87@gmail.com>
2024-02-29 14:21:45 -0800

    cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the URL, and displays the body of the response
+# Sends a GET request to the URL, and display the body of the response
 curl -sX GET "$1" -H "X-School-User-Id: 98" -L
