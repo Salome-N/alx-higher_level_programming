3f2accf3d65cdbeb1fa3b4d728d4590e4b26eabb
Salome-N <njengasalome87@gmail.com>
2024-03-05 18:55:43 -0800

    Fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     url = argv[1]
     value = {"email": argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
+
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
         print(response.read().decode("utf-8"))
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 ''' displays the body of the response '''
 import sys import argv
-import urllib.error
+import urllib.error import HTTPError
 import urllib.request
 
 
 if __name__ == '__main__':
     url = argv[1]
+
     request = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(request) as response:
             print(response.read().decode("ascii"))
-    except urllib.error.HTTPError as e:
+    except urllib.error as e:
         print("Error code: {}".format(e.code))
