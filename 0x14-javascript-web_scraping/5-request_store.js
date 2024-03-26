023a877ec56b8444808be37e5b2fae6bd549007e
Salome-N <njengasalome87@gmail.com>
2024-03-26 15:45:12 -0700

    Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const fs = require('fs');
-request(process.argv[2], function (error, response, body) {
+request(process.argv[2], function (_error, response, body) {
   fs.writeFile(process.argv[3], body, 'utf8', function (err) {
     if (err) {
       console.log(err);
