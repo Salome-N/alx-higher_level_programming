557c687b37fe3cc69c8f2d2e638a18860128b943
Salome-N <njengasalome87@gmail.com>
2024-03-26 15:44:53 -0700

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,18 +4,18 @@ const request = require('request');
 const url = process.argv[2];
 let n = 0;
 
-request(url, function (err, res, body) {
-  result = JSON.parse(body).results;
-  for (let l = 0; l < result.length; ++l) {
-    const chars = result[l].characters;
+request(url, function (_err, res, body) {
+  body = JSON.parse(body).results;
+  for (let l = 0; l < body.length; ++l) {
+    const chars = body[l].characters;
     for (let ln = 0; ln < chars.length; ++ln) {
       const c = chars[ln];
       const charId = c.split('/')[5];
       if (charId === '18') {
-	n += 1;
+        n += 1;
       }
     }
   }
-  
+
   console.log(n);
 });
